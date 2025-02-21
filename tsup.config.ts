8bfc861d77c8a33f9dd5c9abb0f4bbf491ef9eff
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2025-02-21 13:42:30 -0300

    Update tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/**/*.{ts,tsx}'],
+  entry: [
+    'src/server.ts',
+    'src/routes/**/*.ts',
+    'src/functions/**/*.ts',
+    'src/drizzle/**/*.ts',
+    'src/redis/**/*.ts',
+  ],
   clean: true,
   format: 'esm',
   outDir: 'dist',
+  noExternal: ['drizzle-orm'],
 })
