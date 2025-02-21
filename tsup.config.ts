58d530c26cdfa3e46b6e54768c1b353ed4dbc4f6
Leonardo Oliveira <71794931+leotilt@users.noreply.github.com>
2025-02-21 13:43:55 -0300

    update 1

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,10 +3,12 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: [
     'src/server.ts',
+    'src/env.ts',
     'src/routes/**/*.ts',
     'src/functions/**/*.ts',
-    'src/drizzle/**/*.ts',
-    'src/redis/**/*.ts',
+    'src/drizzle/client.ts',
+    'src/drizzle/schema/**/*.ts',
+    'src/redis/**/*.ts'
   ],
   clean: true,
   format: 'esm',
